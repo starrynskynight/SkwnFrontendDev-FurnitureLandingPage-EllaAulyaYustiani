6176bbdfe2340a19560184359306c1333674f488
starrynskynight <ellayustiani@gmail.com>
2025-05-19 16:20:31 +0700

    Furniture Page ref2

--- src/components/PartneredBrand.jsx
+++ src/components/PartneredBrand.jsx
@@ -7,11 +7,11 @@ const PartneredBrands = () => {
           <p className="text-sm md:text-2xl tracking-wide text-[#2F241F] mt-1">PARTNERED BRANDS</p>
         </div>
 
-        <div className="flex items-center justify-center md:justify-start flex-wrap gap-6 md:gap-18">
-          <img src="/brands/gusto.svg" alt="Gusto" className="h-7 md:h-20 object-contain" />
-          <img src="/brands/stripe.svg" alt="Stripe" className="h-7 md:h-20 object-contain" />
-          <img src="/brands/treehouse.svg" alt="Treehouse" className="h-7 md:h-20 object-contain" />
-          <img src="/brands/upwork.svg" alt="Upwork" className="h-7 md:h-20 object-contain" />
+        <div className="flex items-center justify-center md:justify-start flex-wrap gap-6 md:gap-14">
+          <img src="/brands/gusto.svg" alt="Gusto" className="h-7 md:h-18 object-contain" />
+          <img src="/brands/stripe.svg" alt="Stripe" className="h-7 md:h-18 object-contain" />
+          <img src="/brands/treehouse.svg" alt="Treehouse" className="h-7 md:h-18 object-contain" />
+          <img src="/brands/upwork.svg" alt="Upwork" className="h-7 md:h-18 object-contain" />
         </div>
       </div>
     </div>
